dfa104a1714ea65687a76ee3f0c85bbfc130af08
gibsonfriedman <gibsonafriedman@gmail.com>
2024-04-09 22:47:30 -0700

    test

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -10,9 +10,9 @@ echo "$files" | time parallel sh load_denormalized.sh
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:1358/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:1359/ --inputs
